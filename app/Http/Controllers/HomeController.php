e26900638abd8560f6dff9de18bb2c443f48cfa3
Tayberi Evgeny <tayberi00@gmail.com>
2023-11-19 12:48:44 +0500

    Init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class HomeController extends Controller
+{
+    public function __invoke(): Factory|View|Application
+    {
+        return view('home');
+    }
+}
